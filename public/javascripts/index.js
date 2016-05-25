5b218eeeaaad7f150847f007c03d9e997488871c
jianshentan <jianshentan@gmail.com>
2016-05-25 06:12:32 +0000

    removed upload image from phone ability

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -54,33 +54,15 @@ document.getElementById("download-canvas")
 
 // handler for image-upload from local
 function handleImage(e){
-  if (window.FileReader) {
-    if (mobile) {
-      var mpImg = new MegaPixImage(e.target.files[0]);  
-      var backCanvas = document.getElementById('canvas-back');
-      var frontCanvas = document.getElementById('canvas-front');
-      mpImg.render(backCanvas, { width: 500, height: 500, orientation: 8 });
-      mpImg.render(frontCanvas, { width: 500, height: 500, orientation: 8 });
-      var mpImgDataURL = frontCanvas.toDataURL();
-      
-      new MeatImage(mpImgDataURL);
-      $(".sc-camera").hide();
-      $(".uploadModal").modal('hide');
-      
-    } else {
-      var reader = new FileReader();
-      
-      reader.onload = function(event){
-        new MeatImage(event.target.result);
-        $(".sc-camera").hide();
-        $(".uploadModal").modal('hide');
-      };
-     
-      reader.readAsDataURL(e.target.files[0]);     
-    }
-  } else {
-    alert("File Reader not supported");
-  }
+  var reader = new FileReader();
+  
+  reader.onload = function(event){
+    new MeatImage(event.target.result);
+    $(".sc-camera").hide();
+    $(".uploadModal").modal('hide');
+  };
+ 
+  reader.readAsDataURL(e.target.files[0]);     
 }
 
 // handler for image-upload from URL
